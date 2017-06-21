1811401d3763475312b65c946d21c004b28be4de
luiizHF <luiiz0828@gmail.com>
2017-06-21 13:23:00 -0600

    Vista Home
    Vista home para solucionar

--- proyectoBAD/Controllers/HomeController.cs
+++ proyectoBAD/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using proyectoBAD.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,22 @@ namespace proyectoBAD.Controllers
 {
     public class HomeController : Controller
     {
+        //TODO: Poner las anotaciones con respecto a quien debe tener permisos para acceder
+        private proyectoBADEntities db = new proyectoBADEntities();
         // GET: Home
         [Authorize]
         public ActionResult Index()
         {
+            int compra = db.compras.Count();
+            int equipo = db.equipos.Count();
+            int bitacora = db.bitacora_mantenimiento.Count();
+            int solicitud = db.solicitudes.Count();
+
+            ViewBag.Dato1 = compra;
+            ViewBag.Dato2 = equipo;
+            ViewBag.Dato3 = bitacora;
+            ViewBag.Dato4 = solicitud;
+
             return View();
         }
     }
